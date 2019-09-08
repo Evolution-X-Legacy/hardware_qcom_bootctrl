LineageOS/android_hardware_qcom_bootctrl

Add sdm660 to the filter
Change-Id: I9514bc0a0b5571c9820765f602f89ad33b7fd67e

Android.mk
@@ -1,7 +1,7 @@
 ifeq ($(AB_OTA_UPDATER),true)
 # TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
 ifneq ($(BOARD_IS_AUTOMOTIVE),true)
-ifneq ($(filter msm8953 msm8996 msm8998 sdm845,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8953 msm8996 msm8998 sdm660 sdm845,$(TARGET_BOARD_PLATFORM)),)
 LOCAL_PATH := $(call my-dir)
 
 # HAL Shared library for the target. Used by libhardware.

Evolution-X-Legacy/hardware_qcom_bootctrl

Android.mk
@@ -1,5 +1,5 @@
 # Preset TARGET_USES_HARDWARE_QCOM_BOOTCTRL for existing platforms.
-ifneq ($(filter msm8953 msm8996 msm8998 sdm710,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8953 msm8996 msm8998 sdm660 sdm710,$(TARGET_BOARD_PLATFORM)),)
 TARGET_USES_HARDWARE_QCOM_BOOTCTRL := true
 endif
 